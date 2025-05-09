a9ea0d47b94285bdba11d0b1f3a8a96ee1acacea
ArmanRuhit <armanruhit09@gmail.com>
2025-05-09 23:36:36 +0600

    add structured logs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"log/slog"
 	"net/http"
 	"strconv"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "GO")
 	// Initialize a slice containing the paths to the two files. It's important to note that the file containing our base template must be the *fist* file in the slice.
 	files := []string{
@@ -18,21 +19,27 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print("Failed to parse template for method home(): ", err.Error())
+		// log.Print("Failed to parse template for method home(): ", err.Error())
+		// Beacause this home handler is now a method against the application struct it can access its fields, including the structured logger. We will use this to create a log entry at the error level containing the error message, also including the request method and URI as attributes to assist with debugging.
+		app.logger.Error("Failed to parse template for method home()", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("error", err.Error()))
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print("Failed to execute template for method home(): ", err.Error())
+		// log.Print("Failed to execute template for method home(): ", err.Error())
+		// New log process
+		app.logger.Error("Failed to execute template for method home()", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("error", err.Error()))
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
 // SnippetView add a snippetView handler function
-func SnippetView(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetView handler function to accept a pointer to the application struct as a parameter. This allows us to access the application-wide dependencies (like the logger) from within the handler function.
+func (app *application) SnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
+		app.logger.Error("Invalid snippet ID", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("error", err.Error()))
 		http.NotFound(w, r)
 		return
 	}
@@ -42,13 +49,16 @@ func SnippetView(w http.ResponseWriter, r *http.Request) {
 
 	_, errMsg := fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	if errMsg != nil {
-		log.Fatal("Failed to write response for method snippetView(): ", errMsg)
+		// log.Fatal("Failed to write response for method snippetView(): ", errMsg)
+		// New log process
+		app.logger.Error("Failed to write response for method snippetView()", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("error", errMsg.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
 // SnippetCreate add a snippetCreate handler function
-func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) SnippetCreate(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("Display a form for creating a new snippet..."))
 	if err != nil {
 		log.Fatal("Failed to write response for method snippetCreate(): ", err)
@@ -57,12 +67,15 @@ func SnippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 // SnippetCreatePost Add a snippetCreatePost handler function
-func SnippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) SnippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "GO")
 	w.WriteHeader(http.StatusCreated)
 	_, err := w.Write([]byte("Save a new snippet..."))
 	if err != nil {
-		log.Fatal("Failed to write response for method snippetCreatePost(): ", err)
+		// log.Fatal("Failed to write response for method snippetCreatePost(): ", err)
+		// New log process
+		app.logger.Error("Failed to write response for method snippetCreatePost()", slog.String("method", r.Method), slog.String("uri", r.URL.RequestURI()), slog.String("error", err.Error()))
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,65 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies for the web application. For now, we only include the structured logger but we will add more to this as the build proogresses.
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+
+	// Define a new command-line flag with the name "addr", a default value of ":4000" and some short help text to explaining what the flag controls. The value of the flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() funtion to parse the command-line flags. This reads in the command-line flag value and assigns it to the addr variable. You need to call this *before* you use the addr variable, otherwise it wll always contain the default value of ":4000". If any errors are encountered during parsing, the application will be terminated.
+	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which writes to the standard out stream and uses default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		AddSource: true,
+	}))
+
+	// Initialize a new instance of the application struct, passing in the logger as a dependency. This allows us to access the logger from within the application struct methods.
+	app := &application{
+		logger: logger,
+	}
+
 	// Use the http.NewServeMux() function to initialize a new servemux, then register the home function as the handler for the "/" url pattern
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", Home)
-	mux.HandleFunc("GET /snippet/view/{id}", SnippetView)
-	mux.HandleFunc("GET /snippet/create", SnippetCreate)
-	mux.HandleFunc("POST /snippet/create", SnippetCreatePost)
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.Home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.SnippetView)
+	mux.HandleFunc("GET /snippet/create", app.SnippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.SnippetCreatePost)
 
 	// Print a log message to say that the server is starting
-	log.Print("starting server on :4000")
+	// log.Printf("starting server on http://localhost%s", *addr)
+	// Use the info method to log the starting server message at the Info severity (along with listen address as an attribute).
+	logger.Info("starting server", slog.String("addr", *addr))
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in two parameters: the TCP network address to listen on (in this case ":4000") and the servemux we just created. if http.ListenAndServe() returns an error we use the log.Fatal() function to log the error message and exit. Note that any error returned by http.ListenAndServe() is always non-nil.
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// Pass the dereferrenced addr pointer to http.ListenAndServe() so that the value of the flag is used, rather than the default value.
+	err := http.ListenAndServe(*addr, mux)
+	// log.Fatal(err)
+	// And we also use the Error() method to log any error message returnedd by http.ListenAndServe() at the Error severity (along with the error message as an attribute). and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+	os.Exit(1)
+
 }
